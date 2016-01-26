e43e88094d5df2bd1fc91ff6a958a069f62582e3
gavinfay <gfay42@gmail.com>
2016-01-26 15:10:29 -0500

    lab 1

---
+++ labs/bits.r
@@ -0,0 +1,6 @@
+The first ten Fibonacci sequence,  0,1,1,2,3,5,8,13,21,34
+
+`apply` and `tapply` can save you heaps of time and code. It is worth getting familiar with how these functions work.
+
+
+
---
+++ labs/lab1.R
@@ -0,0 +1,8 @@
+# Lab 1 code
+
+2+2
+x <- 1:10
+y <- rnorm(10)
+plot(y~x,pch=16,ylim=c(-2,2))
+abline(h=0,col=gray(0.2))
+
